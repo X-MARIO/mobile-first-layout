54050e0812974257bc189d7cdcbf6058b4eddede
pchekushkin <pchekushkin@clink.tech>
2023-04-14 19:46:39 +0400

    start state

---
+++ webpack.config.js
@@ -0,0 +1,120 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+const isProduction = process.env.NODE_ENV === 'production';
+
+const optimization = () => {
+
+    const config = {
+        splitChunks: {
+            chunks: "all",
+        }
+    }
+
+    if (isProduction) {
+        config.minimizer = [
+            new TerserPlugin(),
+            new CssMinimizerPlugin(),
+        ]
+    }
+
+    return config;
+}
+
+const filename = (ext) => {
+    return isDevelopment ? `[name].${ext}` : `[name].[hash].${ext}`;
+}
+
+const cssLoaders = (ext) => {
+    const loaders = [
+        {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+                // hrm: isDevelopment,
+                // reloadAll: true,
+            },
+        },
+        'css-loader'
+    ];
+
+    if (ext) {
+        loaders.push(ext);
+    }
+
+    return loaders;
+}
+
+const plugins = () => {
+    const base = [
+        new HTMLWebpackPlugin({
+            template: "./index.html",
+            minify: {
+                collapseWhitespace: isProduction,
+            }
+        }),
+        new CleanWebpackPlugin(),
+        // new CopyWebpackPlugin({
+        //     patterns: [
+        //         {
+        //             from: path.resolve(__dirname, 'src/favicon.ico'),
+        //             to: path.resolve(__dirname, 'dist'),
+        //         }
+        //     ]
+        // }),
+        new MiniCssExtractPlugin({
+            filename: filename('css'),
+        })
+    ];
+
+    if (isProduction) {
+        base.push(new BundleAnalyzerPlugin());
+    }
+
+    return base;
+}
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: ['./js/main.js'],
+    },
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: optimization(),
+    devServer: {
+        port: 4200,
+        hot: isDevelopment,
+    },
+    devtool: isDevelopment ? 'source-map' : undefined,
+    plugins: plugins(),
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: cssLoaders(),
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader'),
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                type: 'asset/resource',
+            }
+        ]
+    }
+}
