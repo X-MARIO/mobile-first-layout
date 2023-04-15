ccc7dd3302437133c8f4d5f4e22558bb99832b54
pchekushkin <pchekushkin@clink.tech>
2023-04-15 08:37:57 +0400

    Добавил стили для блока header

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import './css/reset.css';
 import './css/normalize.css';
 import './css/style.css';
 import './scss/style.scss';
-import './scss/var.scss';
+import './scss/vars.scss';
 import './scss/fonts.scss';
